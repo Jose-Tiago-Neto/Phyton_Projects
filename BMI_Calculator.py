fa4c85c5ca8f7d62fbb25f8582110811f9d99840
Jose Tiago Neto <164864868+Jose-Tiago-Neto@users.noreply.github.com>
2024-04-06 19:05:27 +0100

    Python file that takes a user's body mass and height and calculates their BMI.
    Body mass index (BMI) is a value derived from the mass (weight) and height of a person.

---
+++ BMI_Calculator.py
@@ -0,0 +1,6 @@
+user_mass = int(input("Enter year of your body mass "))
+user_height = float(input("Enter year of your height "))
+total_height = (user_height **2)
+bmi = (user_mass/total_height)
+
+print("Your BMI is", bmi)
